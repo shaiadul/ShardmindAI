826004da70914e6f998fbcffa08d1fb4950aea37
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-12-07 15:39:12 +0600

    dashbord sent successfully

--- components/authComponents/Otp/index.jsx
+++ components/authComponents/Otp/index.jsx
@@ -74,7 +74,7 @@ const OtpVerify = () => {
                   type="submit"
                   className="w-full flex justify-center bg-purple-800  hover:bg-purple-700 text-gray-100 p-3  rounded-lg tracking-wide font-semibold  cursor-pointer transition ease-in duration-500"
                 >
-                  Confirm Password
+                  Confirm 
                 </button>
               </div>
             </div>
