c94d8e2e028f96c360d0f82c749f65d4d86485a0
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-12-21 22:13:58 +0600

    dyploy test 2

--- app/page.js
+++ app/page.js
@@ -1,10 +1,3 @@
-"use client";
-
 export default function Home() {
-  if (localStorage.getItem("token") === null)
-    window.location.replace("/authentication/signin");
-
-  window.location.replace("/authentication/signin");
-
   return <main></main>;
 }
